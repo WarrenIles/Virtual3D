e2fad180a7377bdf05aa8905dbd76eb8bc0ce6b6
WarrenIles <warren.iles@yahoo.com>
2024-11-25 12:39:14 -0500

    Added constructor function for faceFinder

--- objectoriented.py
+++ objectoriented.py
@@ -1,4 +1,20 @@
 # This will be an object oriented version
 # of the virtual 3d game.
 
+
+
+class FaceFinder:
+	""" Will utilize haar cascade filter to detect largest face from a frame """
+
+
+	def __init__(self):
+		print('FaceFinder initialize')
+
+
+#--------------------------------------------
+#main
+#
 print('starting oo virtual3d')
+
+ff = FaceFinder()
+print('virtual3d complete')
