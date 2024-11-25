3cff94a518a4dc4f9fbe16a572f91a1e3a703364
WarrenIles <warren.iles@yahoo.com>
2024-11-25 13:18:36 -0500

    Added Facedetection function: Fix tab issues

--- objectoriented.py
+++ objectoriented.py
@@ -1,6 +1,7 @@
 # This will be an object oriented version
 # of the virtual 3d game.
-
+import cv2
+import numpy as np
 
 
 class FaceFinder:
@@ -9,7 +10,31 @@ class FaceFinder:
 
 	def __init__(self):
 		print('FaceFinder initialize')
+		self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+
+	def find_face(self, frame):
+		""" Returns face center (x,y), draws rect on frame """
+		# Convert into grayscale
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		# Detect faces
+		#   detectMultScale returns an 2d ndarray
+		faces = self.face_cascade.detectMultiScale(gray, MinNeighbors = 9)
+		print('detected face(s) at:', faces)
+
+		# Draw rectangle around the faces
+		if faces is None:
+			return None
+			
+		bx=by=bw=bh = 0
+
+		for (x, y, w, h) in faces:
+			if w > bw:  # is current face bigger than biggest so far
+				bx,by,bw,bh = x,y,w,h
+  		cv2.rectangle(frame, (bx, by), (bx+bw, by+bh), (0, 255, 255), 3)
+  		return ((bx+bw//2), by+bh//2))
+  		
 
 #--------------------------------------------
 #main
